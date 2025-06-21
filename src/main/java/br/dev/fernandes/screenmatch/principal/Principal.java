16eea712eb5e29ac119a6107f666b09f62e7af65
devFernandes <fernandes.campos.dev@gamil.com>
2025-06-21 17:45:36 -0300

    stream method

--- src/main/java/br/dev/fernandes/screenmatch/principal/Principal.java
+++ src/main/java/br/dev/fernandes/screenmatch/principal/Principal.java
@@ -3,17 +3,19 @@ package br.dev.fernandes.screenmatch.principal;
 import br.dev.fernandes.screenmatch.model.DadosEpisodio;
 import br.dev.fernandes.screenmatch.model.DadosSerie;
 import br.dev.fernandes.screenmatch.model.DadosTemporada;
+import br.dev.fernandes.screenmatch.model.Episodio;
 import br.dev.fernandes.screenmatch.service.ConsumoApi;
 import br.dev.fernandes.screenmatch.service.ConverteDados;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
-    private Scanner leiture = new Scanner(System.in);
+    private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
     private ConverteDados conversor = new ConverteDados();
 
@@ -22,28 +24,55 @@ public class Principal {
 
     public void exibeMenu() throws IOException, InterruptedException {
         System.out.println("Digite o nome da série para a busca!");
-        var nomeSerie = leiture.nextLine().replaceAll(" ", "+");
+        var nomeSerie = leitura.nextLine().replaceAll(" ", "+");
         var json = consumo.obterDados(ENDERECO + nomeSerie + API_KEY);
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumo.obterDados(ENDERECO + nomeSerie + "&season=" + i +  API_KEY);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
 
-		temporadas.forEach(System.out::println);
-
-//        for (int i = 0; i < dados.totalTemporadas(); i++) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (DadosEpisodio dadosEpisodio : episodiosTemporada) {
-//                System.out.println(dadosEpisodio.titulo());
-//            }
-//        }
+        temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data de lançamento: " + e.getDataLancamento()
+                ));
     }
 }
